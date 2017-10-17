8137b7a7abd4ec5099ae3a2390d36ada48cd5029
Rezina Koudelkova <terezanadejova@gmail.com>
2017-10-17 14:40:49 +0200

    Pridana nova uloha, zatim nefunguje

---
+++ vypisradky.py
@@ -0,0 +1,11 @@
+# Pomocí cyklu for napiš program, který vypíše toto:
+# Řádek 0
+# Řádek 1
+# Řádek 2
+# Řádek 3
+# Řádek 4
+# Funguje? Dej to do Gitu
+
+for radek in range(4):
+    cisloradku = 0
+    print("Radek", cisloradku + 1)
