b2d54ed94fff51dfd29286c36f2172bc825f7a55
Rezina Koudelkova <32848284+rezinak@users.noreply.github.com>
2017-10-20 14:36:17 +0200

    konflikt

--- pyramidax.py
+++
@@ -1,9 +0,0 @@
-# Napiš program, který postupně z jednotlivých 'X' vypíše:
-# X
-# X X
-# X X X
-# X X X X
-# Funguje? Do Gitu s tím!
-for pocetradek in range(1, 5):
-    #for pocetx in range(1):
-    print(pocetradek * " X")
