c66d0edb129baa2002ab6c7d536501d7448ccb93
Rezina Koudelkova <32848284+rezinak@users.noreply.github.com>
2017-10-20 14:31:25 +0200

    Add files via upload

---
+++ 04_09_xka.py
@@ -0,0 +1,19 @@
+# Pomocí cyklů for a parametru end pro print napiš program, který postupně z jednotlivých 'X' vypíše:
+# X X X X X
+# X X X X X
+# X X X X X
+# X X X X X
+# X X X X X
+# „Z jednotlivých 'X'“ znamená, že nepoužiješ např. print('X X X X X').
+# Jak pojmenuješ proměnnou cyklu? A tu druhou?
+
+
+# for pocetRadku in range (5):
+#     for naRadek in range(5):
+#         print(" X ", end = "")
+#     print("")
+
+
+
+for pocetRadku in range(5):
+    print("X " * 5)
---
+++ 04_12_vypisradky.py
@@ -0,0 +1,12 @@
+# Pomocí cyklu for napiš program, který vypíše toto:
+# Řádek 0
+# Řádek 1
+# Řádek 2
+# Řádek 3
+# Řádek 4
+# Funguje? Dej to do Gitu
+cisloradku = 0
+
+for radek in range(5):
+    print("Radek", cisloradku)
+    cisloradku = cisloradku + 1
